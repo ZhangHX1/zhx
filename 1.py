28e47410f0ff66a07b58baee3b1305ffd013c6db
zhx <1144930175@qq.com>
2019-01-15 11:41:23 +0800

    1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,21 @@
 print("hello world!")
+
+#在目标目录中打开终端
+#$ git config --global user.name "Morvan Zhou"
+
+#$ git config --global user.email "mz@email.com"
+
+#$ git init
+# Initialized empty Git repository in /Users/MorvanZhou/Desktop/gitTUT/.git/
+
+#$ touch filename.py
+
+#$ git add 1.py
+
+#$ git status ##查看版本库状态
+
+#输入生成网址两条命令
+
+#$git commit -am 1.py
+
+#git push -u origin master
